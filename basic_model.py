23d658e08d66f016ed9656556923a838d39b6134
pabloralves <pabloralves@protonmail.com>
2024-06-12 15:30:42 +0200

    Starting point for the bee, hive and resource agents

--- basic_model.py
+++ basic_model.py
@@ -7,22 +7,30 @@ import mesa
 
 
 class Bee(mesa.Agent):
-    def __init__(self, age,location):
-        super().__init__(age,location)
-        self.age = 0                                # Float: number of days
-        self.health = 0                             # Float: [0,1]
-        self.location = location                    # (x,y) tuple
+    def __init__(self, age,health,caste,location):
+        super().__init__(age,health,caste,location)
+        self.age = age                              # Float: number of days
+        self.health = health                        # Float: [0,1]
+        self.type = caste                           # String: "worker", "forager", "etc..."
+        self.location = location                    # Tuple: (x,y)
         # TODO: Complete
+    
+    # TODO: Add method to die (based on age, health, location)
+    # TODO: Add method to change type
+    # TODO: Add method for every caste
+    # TODO: Add method to make forager put back resource in hive
 
 class Hive(mesa.Agent):
     def __init__(self, location, radius):
         self.location = location                    # (x,y) tuple
         self.radius = radius                        # Beehive has a circle shape, size may change with population
-
+        # TODO: Complete    
+    
 class Resource(mesa.Agent):
     def __init__(self, location, type, quantity, persistent):
         self.location = location                    # (x,y) tuple
         self.type = type                            # "honey", "water",...
         self.quantity = quantity                    # Float: [0,1]
         self.persistent = persistent                # Bool: True if resource lasts forever
+        # TODO: Complete
     
