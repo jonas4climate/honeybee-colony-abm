3cf3d6024a11a94f843dc606af2cde6be75df1d3
pabloralves <pabloralves@protonmail.com>
2024-06-19 14:25:32 +0200

    Bee agent 85% implemented, Hive 85% implemented

--- ContinuousModel/Hive.py
+++ ContinuousModel/Hive.py
@@ -1,6 +1,9 @@
 from mesa import Agent, Model
 
 from typing import Tuple
+from ContinuousModel.Bee import Bee
+from random import random
+
 
 class Hive(Agent):
     
@@ -31,18 +34,54 @@ class Hive(Agent):
         self.young_bees = young_bees
         self.p_new_forager = 0.0                    # TODO: If it's a function of reosurces, then this should be a class method
 
+
+    def feed_bees(self):
+        # Get all young ones as well as foragers around beehive
+        ## Right now this entails bees around beehive up to 1.5*radius
+        bees_in_hive = [other_agent for other_agent in self.model.schedule.agents if other_agent != self and ((other_agent.x - self.location[0])**2 + (other_agent.y - self.location[1])**2)**0.5 <= (self.radius*1.5) and isinstance(other_agent, Bee)]
+
+        for bee in bees_in_hive:
+            # Feed it, recall maximum health and that there should be resources
+            ## TODO: Prioritize hunger ones! Turning water and pollen into bee health
+            ## TODO: Use two resources
+            if bee.health <= 1 and self.nectar > 0.01:
+                bee.health += 0.01
+                self.nectar -= 0.01
+
+    def mature_bees(self):
+        # This entails maturing young bees to foragers with some probability based on resources, weather etc...
+        for young_bee in range(self.young_bees):
+            mature = True if random() < self.p_new_forager else False
+            if mature:
+                new_forager = Bee(self,fov=0.5, age=4, health=1, state="resting", wiggle=False)
+                self.grid.place_agent(new_forager, (0, 0))
+                self.young_bees -= 1
+        
+
+    def update_p_forager(self):
+        ## TODO: Modify probability with resrouces
+        self.p_new_forager = self.p_new_forager
+        
+
+    def create_bees(self):
+        ## TODO: Update probability with resources, weather...
+        p_new_young_bee = 0.1
+        new_young = True if random() < p_new_young_bee else False
+        if new_young:
+            self.young_bees += 1
+
     def step(self):
         # 1. Feed bees
-        # Get all young ones as well as foragers around beehive
-        # Prioritize hunger ones! Turning water and pollen into bee health
-        pass
+        feed_bees(self)
 
         # 2. Mature bees
         # Use p_new_forager to instantiate bees
-        pass
+        mature_bees(self)
+
+
         # Then, update p_new_forager based on young, water, and pollen, and weather!!
-        pass
+        update_p_forager(self)
 
         # 3. Create young bees
         # Based on resources and weather
-        pass
+        create_bees(self)
